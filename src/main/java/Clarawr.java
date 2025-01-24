aed8d755f513e1624b55b1d04a2a74a375327c60
brvndonkoh <e1121709@u.nus.edu>
2025-01-24 11:33:42 +0800

    Add greeting

---
+++ src/main/java/Clarawr.java
@@ -0,0 +1,9 @@
+public class Clarawr {
+    public static void main(String[] args) {
+        String name = "Clarawr";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+

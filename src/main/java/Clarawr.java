dfc302fe63a07c815a63ecf515e6e64b2c34556d
brvndonkoh <e1121709@u.nus.edu>
2025-02-01 13:11:33 +0800

    Clarawr.java: Add save feature

--- src/main/java/Clarawr.java
+++ src/main/java/Clarawr.java
@@ -1,6 +1,15 @@
-import java.io.*;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
+
+enum TaskType {
+    TODO, DEADLINE, EVENT
+}
 
 public class Clarawr {
     static ArrayList<Task> tasks = new ArrayList<>();

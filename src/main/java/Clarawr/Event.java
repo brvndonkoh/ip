64873238b5c5010d35b46f673eb033f296ba6a80
brvndonkoh <e1121709@u.nus.edu>
2025-02-03 21:17:20 +0800

    Add JavaDoc

--- src/main/java/Clarawr/Event.java
+++ src/main/java/Clarawr/Event.java
@@ -3,11 +3,23 @@ package Clarawr;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents an event task with a description, start and end times.
+ * Inherits from the Task class and provides additional functionality for event-specific properties.
+ */
 public class Event extends Task {
     protected LocalDateTime from;
     protected LocalDateTime to;
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM-dd-yyyy HH:mm");
 
+    /**
+     * Constructs an Event task with the given description, start and end times, and completion status.
+     *
+     * @param description The description of the event.
+     * @param from The start time of the event.
+     * @param to The end time of the event.
+     * @param isDone The completion status of the event.
+     */
     public Event(String description, LocalDateTime from, LocalDateTime to, boolean isDone) {
         super(description, TaskType.EVENT);
         this.from = from;
@@ -15,20 +27,41 @@ public class Event extends Task {
         this.isDone = isDone;
     }
 
+    /**
+     * Gets the start time of the event.
+     *
+     * @return The start time of the event.
+     */
     public LocalDateTime getFrom() {
         return from;
     }
 
+    /**
+     * Gets the end time of the event.
+     *
+     * @return The end time of the event.
+     */
     public LocalDateTime getTo() {
         return to;
     }
 
+    /**
+     * Returns a string representation of the event task, including the description,
+     * completion status, and start and end times.
+     *
+     * @return A string representation of the event task.
+     */
     @Override
     public String toString() {
         return " [E]" + super.toString() + " from: " + from.format(formatter) + " to: " +
                 to.format(formatter) ;
     }
 
+    /**
+     * Returns a string representation of the event task in a format suitable for saving to a file.
+     *
+     * @return A string representation of the event task in file format.
+     */
     @Override
     public String toFileString() {
         return "[E]" + super.toFileString() + " /from " + from.format(formatter) + " /to " +
--- src/main/java/Clarawr/Task.java
+++ src/main/java/Clarawr/Task.java
@@ -1,39 +1,84 @@
 package Clarawr;
 
+/**
+ * Represents a generic task with a description, completion status, and type.
+ * Subclasses must specify the type of task (e.g., Todo, Deadline, Event).
+ */
 abstract class Task {
     protected String description;
     protected boolean isDone;
     protected TaskType type;
 
+    /**
+     * Constructs a new task with a description and task type.
+     * The task is initially marked as not done.
+     *
+     * @param description The description of the task.
+     * @param type The type of the task (e.g., Todo, Deadline, Event).
+     */
     public Task(String description, TaskType type) {
         this.description = description;
         this.isDone = false;
         this.type = type;
     }
 
+    /**
+     * Returns the status icon of the task.
+     * "X" represents a completed task, and a space represents an incomplete task.
+     *
+     * @return The status icon of the task, either "X" or a space.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Marks the task as completed.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
+
+    /**
+     * Marks the task as not completed.
+     */
     public void markUndone() {
         this.isDone = false;
     }
 
+    /**
+     * Returns a string representation of the task.
+     * The string includes the task status icon and the description.
+     *
+     * @return A string representation of the task.
+     */
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
     }
 
+    /**
+     * Returns a string representation of the task in a format suitable for file storage.
+     *
+     * @return A string representing the task, formatted for file storage.
+     */
     public String toFileString() {
         return (isDone ? "X" : " ") + description;
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return The description of the task.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Returns whether the task is completed.
+     *
+     * @return True if the task is done, false otherwise.
+     */
     public boolean isDone() {
         return isDone;
     }

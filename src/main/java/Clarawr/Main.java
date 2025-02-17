7dcd69802ec6a6d65e6f382994dc50a37753de3f
brvndonkoh <e1121709@u.nus.edu>
2025-02-17 14:01:19 +0800

    Add Clarawr icon and display name in top-right window

--- src/main/java/Clarawr/Main.java
+++ src/main/java/Clarawr/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -24,6 +25,11 @@ public class Main extends Application {
 	 */
 	@Override
 	public void start(Stage stage) {
+
+		stage.setTitle("Clarawr");
+
+		Image image = new Image("/images/cuteLion.png");
+		stage.getIcons().add(image);
 
 		assert clarawr != null : "Clarawr instance should not be null";
 		try {
